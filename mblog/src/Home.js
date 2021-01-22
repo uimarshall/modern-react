f1007803cd338368deaf0722fe81278be67aaf85
uimarshall <uimarshall@gmail.com>
2021-01-22 16:31:28 +0100

    Add useState

--- mblog/src/Home.js
+++ mblog/src/Home.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { useState } from "react";
 
 const Home = () => {
+	// const page = "Homepage";
+	// Array destructuring
+	const [page, setPage] = useState("Homepage");
 	const handleClick = () => {
-		alert("Hello");
+		setPage("New Homepage");
 	};
+	// const handleClick = () => {
+	// 	alert("Hello");
+	// };
 	const handleClick2 = (name, e) => {
 		alert("Hello " + name, e.timeStamp);
 	};
 	return (
 		<div className="home">
-			<h2>Homepage</h2>
+			<h2>{page}</h2>
 			<button onClick={handleClick}>Click</button>
 			<button onClick={(e) => handleClick2("Marshall", e)}>Click Me</button>
 		</div>
