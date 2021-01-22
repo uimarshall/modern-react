e6aef8c35d2c8581cddb4ac619c6373481b6fd2b
uimarshall <uimarshall@gmail.com>
2021-01-22 15:31:23 +0100

    Add styles

--- mblog/src/Navbar.js
+++ mblog/src/Navbar.js
@@ -1,13 +1,13 @@
 import React from "react";
 const Navbar = () => {
 	return (
-		<div>
+		<nav className="navbar">
 			<h1>The M Blog</h1>
 			<div className="nav-links">
 				<a href="/">Home</a>
 				<a href="/create">New Blog</a>
 			</div>
-		</div>
+		</nav>
 	);
 };
 
