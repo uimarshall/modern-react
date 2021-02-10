76ebf3cba1c2237a9d3cc7cbbaa5a549ef8c23df
uimarshall <uimarshall@gmail.com>
2021-02-10 17:22:39 +0100

    Add delete function

--- mblog/src/BlogDetails.js
+++ mblog/src/BlogDetails.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 import useFetch from './customHooks/useFetch'
 
 const BlogDetails = () => {
@@ -7,6 +7,18 @@ const BlogDetails = () => {
   const { data: blog, error, isLoading } = useFetch(
     'http://localhost:8000/blogs/' + route_params,
   )
+
+  const history = useHistory()
+
+  const handleDelete = () => {
+    fetch('http://localhost:8000/blogs' + blog.id, {
+      method: 'DELETE',
+    }).then(() => {
+      // setIsLoading(false)
+      //   history.go(-1)
+      history.push('/')
+    })
+  }
   return (
     <div className="blog-details">
       <h2>Blog details - {route_params}</h2>
@@ -17,6 +29,9 @@ const BlogDetails = () => {
           <h1>{blog.title}</h1>
           <p>Author: {blog.author}</p>
           <div>{blog.content}</div>
+          <button type="button" onClick={handleDelete}>
+            delete blog
+          </button>
         </article>
       )}
     </div>
